af3f3cd5bb2d3f6df64037f38e813ecc0c806c0b
vahid ara <vahidaara@gmail.com>
2023-12-09 14:04:56 +0330

    complete serialize Data an deserialze data in the form of List

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Data;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int id;
+            public string Name;
+        }
+        delegate int Del(int id);
+  
+        
+        static void Main(string[] args)
+        {
+            List<Audi> listA = new List<Audi>()
+                                    {
+
+                                        new Audi()
+                                        {
+                                            Name = "A1",
+                                            NumberOfCilander=1
+                                        },
+                                        new Audi()
+                                        {
+                                            Name = "A2",
+                                            NumberOfCilander=2
+                                        },
+                                       new Audi()
+                                        {
+                                            Name = "A3",
+                                            NumberOfCilander=3
+                                        },
+
+                                    };
+            Serialization<Audi> serA = new Serialization<Audi>();
+            string FilePathAudi = @"D:\files\JFileAudi.txt";
+            serA.SerializeListOfData(listA, FilePathAudi);
+
+            List<Audi> Audies=serA.Deserialize(FilePathAudi);
+           
+            foreach (Audi au in Audies)
+            {
+                Console.WriteLine(au.Name);
+            }
+            Console.WriteLine("************");
+
+            List<Motor> listM = new List<Motor>()
+                                    {
+
+                                        new Motor()
+                                        {
+                                            Name = "m1",
+                                            NumberOfCilander=1
+                                        },
+                                        new Motor()
+                                        {
+                                            Name = "m2",
+                                            NumberOfCilander=2
+                                        },
+                                       new Motor()
+                                        {
+                                            Name = "M3",
+                                            NumberOfCilander=3
+                                        },
+
+                                    };
+            Serialization<Motor> serM = new Serialization<Motor>();
+            string FilePathMotor = @"D:\files\JFileMotor.txt";
+            serM.SerializeListOfData(listM, FilePathMotor);
+            //change
+            List<Motor> Motorss = serM.Deserialize(FilePathMotor);
+            Motor m = Motorss[0];
+            m.Name = "new Motor";
+            serM.SerializeListOfData(Motorss, FilePathMotor);
+            List<Motor>motorx=serM.Deserialize(FilePathMotor);
+            foreach (Motor au in motorx)
+            {
+                Console.WriteLine(au.Name);
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Serialization.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public  class Serialization<T>
+
+    {
+        //serialize list of object to file
+        public void SerializeListOfData(List<T> ListOfOBJ, string FilePath)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(ListOfOBJ, options);
+            File.WriteAllText(FilePath, jsonString);
+        }
+        //deserialize string  to list object
+        public List<T> Deserialize(string FilePath)
+        {
+            List<T> ListOfObject = new List<T>();
+            string AllObject = File.ReadAllText(FilePath);
+            ListOfObject = JsonConvert.DeserializeObject<List<T>>(AllObject);
+            //ListOfObject = System.Text.Json.JsonSerializer.Deserialize<List<T>>(AllObject);
+            return ListOfObject;
+        }
+       
+
+
+
+
+
+
+
+    }
+}
